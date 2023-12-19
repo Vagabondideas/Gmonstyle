b7a2d8a8659b99be7701631c0dc599cf94a8986b
vagabondideas <whitesectora@gmail.com>
2023-12-19 18:40:38 -0500

    media-1

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 // "use strict";
 
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const contactForm = document.querySelector(".contact-form");
-const btnSubmitMsg = document.querySelector(".btn-submit-msg");
+const navMenu = document.querySelector(".nav-main-menu");
+
+// const contactForm = document.querySelector(".contact-form");
+// const btnSubmitMsg = document.querySelector(".btn-submit-msg");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+    console.log("clicked");
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 });
 
 document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
+    n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    })
 );
 
-btnSubmitMsg.addEventListener("click", function () {
-  contactForm.style.visibility = "hidden";
-});
+// btnSubmitMsg.addEventListener("click", function () {
+//     contactForm.style.visibility = "hidden";
+// });
